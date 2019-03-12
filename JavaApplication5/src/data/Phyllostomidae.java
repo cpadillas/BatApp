1547d4d93ef01f8335127e5e2c614ec1b1d831cf
cpadillas <30636268+cpadillas@users.noreply.github.com>
2019-03-11 19:37:44 -0500

    Primera subida

---
+++ JavaApplication5/src/data/Phyllostomidae.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author crist
+ */
+public class Phyllostomidae extends Murcielago{
+    int Nasal; /* 1=Rudimentaria, 2=Prominente*/
+    int incisivosSuperiores; /* 1=Mas anchos que caninos 2=Menores que los caninos*/
+    int polex; /*1=Bien desarrollado 2=Pequeño*/
+    
+}
